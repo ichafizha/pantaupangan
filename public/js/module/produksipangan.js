bf2a3a5ff47ccc4dcc6061c819386452288f43d5
Muhammad Rivki <Muhammad.Rivki@outlook.com>
2016-08-12 13:03:27 +0700

    add scatter chart

--- public/js/module/produksipangan.js
+++ public/js/module/produksipangan.js
@@ -1,41 +1,67 @@
 'use strict';
 
 (function() {
-    console.log('masuk');
-    var produksi = produksi.data;
-    console.log(produksi);
-    $.get(location.origin + '/api/v1/komoditas/cluster')
-        .then(function(response) {
-            console.log('masuk')
-            console.log(response);
+  console.log('masuk');
 
-            nv.addGraph(function() {
-                var chart = nv.models.scatterChart()
-                    .showDistX(true) //showDist, when true, will display those little distribution lines on the axis.
-                    .showDistY(true)
-                    .transitionDuration(350)
-                    .color(d3.scale.category10().range());
-
-                //Configure how the tooltip looks.
-                chart.tooltipContent(function(key) {
-                    return '<h3>' + key + '</h3>';
-                });
-
-                //Axis settings
-                chart.xAxis.tickFormat(d3.format('.02f'));
-                chart.yAxis.tickFormat(d3.format('.02f'));
-
-                //We want to show shapes other than circles.
-                chart.scatter.onlyCircles(false);
-
-                var myData = response;
-                d3.select('#chart svg')
-                    .datum(myData)
-                    .call(chart);
-
-                nv.utils.windowResize(chart.update);
-
-                return chart;
-            });
+ // register our custom symbols to nvd3
+    // make sure your path is valid given any size because size scales if the chart scales.
+    nv.utils.symbolMap.set('thin-x', function(size) {
+        size = Math.sqrt(size);
+        return 'M' + (-size/2) + ',' + (-size/2) +
+                'l' + size + ',' + size +
+                'm0,' + -(size) +
+                'l' + (-size) + ',' + size;
+    });
+    // create the chart
+    var chart;
+    nv.addGraph(function() {
+        chart = nv.models.scatterChart()
+            .showDistX(true)
+            .showDistY(true)
+            .useVoronoi(true)
+            .color(d3.scale.category10().range())
+            .duration(300)
+        ;
+        chart.dispatch.on('renderEnd', function(){
+            console.log('render complete');
         });
+        chart.xAxis.tickFormat(d3.format('.02f'));
+        chart.yAxis.tickFormat(d3.format('.02f'));
+        d3.select('svg')
+            .datum(randomData(4,40))
+            .call(chart);
+        nv.utils.windowResize(chart.update);
+        chart.dispatch.on('stateChange', function(e) { ('New State:', JSON.stringify(e)); });
+        return chart;
+    });
+    function randomData(groups, points) { //# groups,# points per group
+        // smiley and thin-x are our custom symbols!
+        var data = [],
+            shapes = ['thin-x', 'circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
+            random = d3.random.normal();
+        for (var i = 0; i < groups; i++) {
+            data.push({
+                key: 'Group ' + i,
+                values: []
+            });
+            for (var j = 0; j < points; j++) {
+                data[i].values.push({
+                    x: random(),
+                    y: random(),
+                    size: Math.round(Math.random() * 100) / 100,
+                    shape: shapes[j % shapes.length]
+                });
+            }
+        }
+        return data;
+    }
+
+  $.get(location.origin + '/api/v1/komoditas/cluster')
+    .then(function(response) {
+      console.log('masuk')
+      console.log(response);
+
+      
+
+    });
 })();
